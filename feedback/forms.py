faa0ab5fe5e479234c70f2dccbce2508b14922c1
Alexander-Bakshaev <shuriknt@gmail.com>
2025-03-11 11:50:21 +0500

    Сохранение данных через форму

--- feedback/forms.py
+++ feedback/forms.py
@@ -4,6 +4,6 @@ class FeedbackForm(forms.Form):
     name = forms.CharField(min_length=2, max_length=7,
                            error_messages={'min_length': 'Слишком короткое имя', 'max_length': 'Слишком длинное имя', 'required': 'Обязательное поле'},
                            widget=forms.TextInput(attrs={'placeholder': 'Ваше имя'}), label='Имя')
-    # surname = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'placeholder': 'Ваша фамилия'}))
-    # feedback = forms.CharField(label='Отзыв', widget=forms.Textarea(attrs={'rows': 3, 'cols': 21}))
-    raiting = forms.IntegerField(label='Рейтинг', min_value=1, max_value=10)
+    surname = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'placeholder': 'Ваша фамилия'}))
+    feedback = forms.CharField(label='Отзыв', widget=forms.Textarea(attrs={'rows': 3, 'cols': 21}))
+    rating = forms.IntegerField(label='Рейтинг', min_value=1, max_value=10)
--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    feedback = models.TextField()
+    rating = models.PositiveIntegerField()
