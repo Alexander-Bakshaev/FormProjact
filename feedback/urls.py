6c9949259c3511907f64c7b571b9dd1b101178f4
Alexander-Bakshaev <shuriknt@gmail.com>
2025-03-23 09:58:17 +0500

    CBV

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import FeedbackView, UpdateFeedbackView, DoneView
+from .views import FeedbackView, UpdateFeedbackView, DoneView, ListFeedBackView, DetailFeedBackView
 
 urlpatterns = [
-    path('done', DoneView.as_view()),
     path('', FeedbackView.as_view()),
+    path('done', DoneView.as_view()),
+    path('all', ListFeedBackView.as_view(), name='list_feedback'),
+    path('detail/<int:pk>/', DetailFeedBackView.as_view(), name='detail_feedback'),
     path('<int:feedback_id>/', UpdateFeedbackView.as_view()),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -3,12 +3,28 @@ from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
 from .models import Feedback
 from django.views import View
+from django.views.generic import ListView, DetailView, TemplateView, FormView
 
 
-class FeedbackView(View):
-    def get(self, request):
-        form = FeedbackForm()
-        return render(request, 'feedback/feedback.html', context={'form': form})
+# class FeedbackView(View):
+#     def get(self, request):
+#         form = FeedbackForm()
+#         return render(request, 'feedback/feedback.html', context={'form': form})
+#
+#     def post(self, request):
+#         form = FeedbackForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             form.save()
+#             return HttpResponseRedirect('/done')
+#         else:
+#             return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+class FeedbackView(FormView):
+    form_class = FeedbackForm
+    template_name = 'feedback/feedback.html'
+
 
     def post(self, request):
         form = FeedbackForm(request.POST)
@@ -18,7 +34,6 @@ class FeedbackView(View):
             return HttpResponseRedirect('/done')
         else:
             return render(request, 'feedback/feedback.html', context={'form': form})
-
 
 class UpdateFeedbackView(View):
     def get(self, request, feedback_id):
@@ -37,6 +52,39 @@ class UpdateFeedbackView(View):
             return render(request, 'feedback/feedback.html', context={'form': form})
 
 
-class DoneView(View):
-    def get(self, request):
-        return render(request, 'feedback/done.html')
+# class ListFeedBackView(TemplateView):
+#     template_name = 'feedback/list_feedback.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['feedbacks'] = Feedback.objects.all()
+#         return context
+
+
+class ListFeedBackView(ListView):
+    template_name = 'feedback/list_feedback.html'
+    model = Feedback
+    context_object_name = 'feedbacks'
+
+
+# class DetailFeedBackView(TemplateView):
+#     template_name = 'feedback/detail_feedback.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         feedback_id = kwargs['feedback_id']
+#         feedback = Feedback.objects.get(id=feedback_id)
+#         context['feedback'] = feedback
+#         return context
+
+class DetailFeedBackView(DetailView):
+    template_name = 'feedback/detail_feedback.html'
+    model = Feedback
+
+class DoneView(TemplateView):
+    template_name = 'feedback/done.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Спасибо'
+        return context
